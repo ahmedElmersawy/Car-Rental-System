d4de693b40b2dd8bdbdd23f045827a48813dbdbb
Omar111Essam <omarkhattab220@gmail.com>
2024-05-22 01:53:39 +0300

    يلا pينا

---
+++ Front End/newFile.js
@@ -0,0 +1,18 @@
+<script>
+    let lastScrollTop = 0;
+    const topHeader = document.querySelector('.top-header');
+    const mainHeader = document.querySelector('.header');
+
+    window.addEventListener('scroll', function() {let} scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        if (scrollTop > lastScrollTop) {
+        // Scroll Down
+        topHeader.style.top = '-40px'};
+    mainHeader.style.top = '-60px';
+        } else {
+        // Scroll Up
+        topHeader.style.top = '0'};
+    mainHeader.style.top = '40px';
+        }
+    lastScrollTop = scrollTop;
+    });
+</script>;
